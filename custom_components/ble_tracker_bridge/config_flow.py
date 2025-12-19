4e57d5117363c3048b29028a396bfd996d901caa
plplaaa2 <124797654+plplaaa2@users.noreply.github.com>
2025-12-19 16:13:18 +0900

    Update config_flow.py

--- custom_components/ble_tracker_bridge/config_flow.py
+++ custom_components/ble_tracker_bridge/config_flow.py
@@ -1,42 +1,19 @@
-import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.helpers import selector
-
+import voluptuous as vol
 from .const import DOMAIN, CONF_SENSORS
 
-class BleTrackerBridgeConfigFlow(config_entries.ConfigFlow):
-    """BLE Tracker Bridge config flow."""
-    domain = DOMAIN
+class BleTrackerBridgeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
         if user_input is not None:
-            return self.async_create_entry(title="BLE Tracker Bridge", data=user_input)
+            return self.async_create_entry(
+                title="BLE Tracker Bridge",
+                data={CONF_SENSORS: user_input.get(CONF_SENSORS, [])},
+            )
 
         schema = vol.Schema({
-            vol.Required(CONF_SENSORS): selector.EntitySelector(
-                selector.EntitySelectorConfig(domain="binary_sensor", multiple=True)
-            )
+            vol.Optional(CONF_SENSORS, default=[]): [str],
         })
-        return self.async_show_form(step_id="user", data_schema=schema)
-
-    @staticmethod
-    def async_get_options_flow(config_entry):
-        return BleTrackerBridgeOptionsFlowHandler(config_entry)
-
-
-class BleTrackerBridgeOptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        default_sensors = self.config_entry.options.get(CONF_SENSORS) or self.config_entry.data.get(CONF_SENSORS, [])
-        schema = vol.Schema({
-            vol.Required(CONF_SENSORS, default=default_sensors): selector.EntitySelector(
-                selector.EntitySelectorConfig(domain="binary_sensor", multiple=True)
-            )
-        })
-        return self.async_show_form(step_id="init", data_schema=schema)
+        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
